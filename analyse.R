1faf34ef23c68b60d808d994eb0c61a317686c24
Paul Sharpe <paul@miraclefish.com>
2017-04-07 17:53:35 +0100

    Survey data to long format.

--- analyse.R
+++ analyse.R
@@ -1,30 +1,25 @@
-library('rjson')
 library('plyr')
 library('jsonlite')
 
 setwd("/home/paul/Desktop/pilot/data")
-path <- "p1"
-survey <- "sms"
 
 process_json_files <- function(path, survey) {
-  f <- paste(path,'/', path, '_', survey, '.json', sep='')
-  data <- jsonlite::stream_in(file(f))
-  # use gather or melt
-  #  data <- fromJSON(file=f)
-  
-  df <- data.frame()
-  # a survey is a list of lists
-  ldply(survey, function(l) {
-    Value
-    
-  })
+  f    <- paste(path,'/', path, '_', survey, '.json', sep='')
+  df   <- jsonlite::stream_in(file(f))
+  df$p <- substr(path,2,nchar(path))    # participant number
+
+  df$question     <- as.numeric(apply(df[1], 2, function(x) stringr::str_match(x,'survey_(\\d+)')[,2]))
+  df[,"question"] <- df[,"question"] - 1 # questions start at 2
+  df$name         <- survey
+  df              <- df[, !(names(df) %in% c('text','options'))]
+  names(df)[names(df) == 'name'] <- 'survey'
+  return(df)
 }
 
-bigval <- NULL
-for(x in 1:10) {
-  bigval <- c(bigval,data[[x]]$value)
+process_surveys <- function (s) {
+  return(ldply(paths, process_json_files, s))
 }
 
-
-paths <- list.files(".")
-sms <- ldply(paths, process_json_files, 'sms')
+paths   <- list.files(".")
+surveys <- c('demographics','sms','ffmq')
+df      <- ldply(surveys, process_surveys)
